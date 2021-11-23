ec486b53f147e45642c7ec84f461f5ff52444bf0
ForeytKL22 <krforeyt@gmail.com>
2021-11-22 21:23:28 -0700

    end of lesson 3

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const {
     addComment,
-    removeComment
+    removeComment,
+    addReply,
+    removeReply
 } = require('../../controllers/comment-controller');
 
 
@@ -13,7 +15,13 @@ router
 // /api/comments/<pizzaId>/<commentId>
 router
     .route('/:pizzaId/:commentId')
+    .put(addReply)
     .delete(removeComment)
+
+// /api/comments/<pizzaId>/<commentId>/<replyId>
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply)
 
 
 module.exports = router;
